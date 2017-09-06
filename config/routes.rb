67a46ab0f55b91c108fc8091d04b7daa3b070b6d
vagrant <vagrant@localhost.localdomain>
2017-09-06 11:46:16 +0900

    Fin layout and route

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
 
 
-  get 'static_pages/help'
-  get 'static_pages/about'
+  get 'signup'=>'users#new'
+
+  get 'help'=>'static_pages#help'
+  get 'about'=>'static_pages#about'
+  get 'contact'=>'static_pages#contact'
   root 'static_pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
