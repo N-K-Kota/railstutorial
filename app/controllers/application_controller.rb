dc4ebd659078529456aa07a2695fb0dddcd43402
vagrant <vagrant@localhost.localdomain>
2017-10-07 12:55:26 +0900

    finlog/logout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,5 @@
 class ApplicationController < ActionController::Base
+  include SessionsHelper
   protect_from_forgery with: :exception
-  def log_in(user)
-    session[:user_id]=user.id
-  end
-  def log_out
-    session.delete(:user_id)
-    @current_user=nil
-  end
+  
 end
