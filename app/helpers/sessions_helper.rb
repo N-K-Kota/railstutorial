0b09e815f506901a64606ddccc387d23311d72bb
vagrant <vagrant@localhost.localdomain>
2017-10-21 08:30:59 +0900

    Finish user edit, update, index, and destroy actions

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -4,11 +4,14 @@ module SessionsHelper
     @current_user||=User.find_by(id:session[:user_id])
   elsif(user_id=cookies.signed[:user_id])
       user=User.find_by(id:user_id)
-      if user && user.authenticate?(cookies[:remember_token])
+      if user && user.authenticated?(cookies[:remember_token])
         log_in user
         @current_user=user
       end
     end
+  end
+  def current_user?(user)
+    user==current_user
   end
   def log_in(user)
     session[:user_id]=user.id
@@ -30,6 +33,13 @@ module SessionsHelper
     user.remember
     cookies.permanent.signed[:user_id]=user.id
     cookies.permanent[:remember_token]=user.remember_token
+  end
+  def redirect_back_or(default)
+    redirect_to(session[:forwarding_url]||default)
+    session.delete(:forwardin_url)
+  end
+  def store_location
+    session[:forwarding_url]=request.url if request.get?
   end
 
 end
--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -4,12 +4,19 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
   # test "the truth" do
   #   assert true
   # end
+  def setup
+    @user=users(:michael)
+  end
   test "layout links" do
-    get root_path
-    assert_template 'static_pages/home'
-    assert_select "a[href=?]", root_path, count: 2
-    assert_select "a[href=?]", help_path
-    assert_select "a[href=?]", about_path
-    assert_select "a[href=?]", contact_path
+     get root_path
+     assert_template 'static_pages/home'
+     assert_select "a[href=?]", root_path, count: 2
+     assert_select "a[href=?]", help_path
+     assert_select "a[href=?]", about_path
+     assert_select "a[href=?]", contact_path
+     log_in_as(@user)
+     get users_path
+     assert_template 'users/index'
+     assert_select "a[href=?]",users_path
   end
 end
